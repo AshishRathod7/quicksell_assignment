e8c4a02fa2e4c1e1ebb86ef90f850a03953c49a7
Ashish Rathod <137209332+AshishRathod7@users.noreply.github.com>
2024-10-09 23:39:38 +0530

    Update FeatureCard.js

--- src/components/Card/FeatureCard.js
+++ src/components/Card/FeatureCard.js
@@ -15,7 +15,7 @@ const FeatureRequestCard = ({ id, title, tag,grouping }) => {
         <div className={styles.content}>
           <h2 className={styles.title}>{title}</h2>
           <div  className={styles.inside}>
-            <div className={styles.nafl}>    ...   </div>
+            {grouping === "status" &&<div className={styles.nafl}>  ...  </div>}
             <div className={styles.dot}></div>
             <button className={styles.featureRequestButton}> {tag} </button>
           </div>
